8c3f5e681dd459cfd7da831cb6834d22bc201f39
shishuai_cd <shishuai_cd@keruyun.com>
2018-03-13 10:33:12 +0800

    stage1 全局变量

--- src/index.js
+++ src/index.js
@@ -9,3 +9,22 @@ const appState = {
   }
 }
 
+function renderApp (appState) {
+  renderTitle(appState.title)
+  renderContent(appState.content)
+}
+
+function renderTitle (title) {
+  const titleDOM = document.getElementById('title')
+  titleDOM.innerHTML = title.text
+  titleDOM.style.color = title.color
+}
+
+function renderContent (content) {
+  const contentDOM = document.getElementById('content')
+  contentDOM.innerHTML = content.text
+  contentDOM.style.color = content.color
+}
+
+renderApp(appState)
+
