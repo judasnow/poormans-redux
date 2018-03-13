61bacadc1d973d55831fe49a68e50e595fc70c66
shishuai_cd <shishuai_cd@keruyun.com>
2018-03-13 10:56:52 +0800

    将对全局变量的修改集中到一个单独的方法中

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import App from "./App";
+
 const appState = {
   title: {
     text: 'title1',
@@ -6,6 +8,23 @@ const appState = {
   content: {
     text: 'text2',
     color: 'blue'
+  }
+}
+
+// 直接使用全局变量中的值
+// 但是全局变量在 app 中无法得到保障
+
+// 为了解决这个问题 定义一个方法 将所有对全局状态的修改
+// 都集中在同一个地方
+// 根据不同的 action 执行不同的操作
+function dispatch (action) {
+  switch (action.type) {
+    case 'update_title':
+      appState.title.text = action.text
+      break;
+    case 'update_content':
+      appState.content.text = action.text
+      break;
   }
 }
 
@@ -27,4 +46,5 @@ function renderContent (content) {
 }
 
 renderApp(appState)
-
+dispatch({ type: 'update_title', text: '《React.js 小书》' })
+renderApp(appState)
