b0a8b8d8e012272a093b7a38df3c3c3a99394d30
judasnow <judasnow@gmail.com>
2018-04-01 17:05:12 +0800

    some changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,86 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import {createStore} from './redux'
+import {connect} from './redux-react'
+
+const themeReducer = (state, action) => {
+  if (!state) return {
+    themeColor: 'blue'
+  }
+  switch (action.type) {
+    case 'CHANGE_COLOR':
+      return { ...state, themeColor: action.themeColor }
+    default:
+      return state
+  }
+}
+
+class Header extends Component {
+  // static contextTypes = {
+  //   store: () => {}
+  // }
+
+  // contextTypes = {
+  //   themeColor: () => true
+  // }
+
+  constructor () {
+    super()
+    // this.state = { themeColor: '' }
+  }
+
+  // componentWillMount () {
+  //   this._updateThemeColor()
+  // }
+  //
+  // _updateThemeColor () {
+  //   // 使用 context 中的状态设置自己的状态
+  //   const { store } = this.context
+  //   const state = store.getState()
+  //   console.dir(store)
+  //   this.setState({ themeColor: state.themeColor })
+  // }
+
+  render () {
+    return <div>header: {this.props.themeColor}</div>
+  }
+}
+
+let mapStateToProps = (state, ownProps) => {
+  return {
+    themeColor: state.themeColor
+  }
+}
+let HeaderCon = connect(mapStateToProps)(Header)
+
+const store = createStore(themeReducer)
+
+class WrapNode extends Component {
+  static childContextTypes = {
+    store: () => true
+  }
+
+  constructor () {
+    super()
+  }
+
+  getChildContext() {
+    return {store: store};
+  }
+
+  render () {
+    return <div>{this.props.children}</div>
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <WrapNode>
+          <HeaderCon />
+        </WrapNode>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,70 +1,9 @@
-import React, {Component} from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom'
-import Header from './Header'
-import Content from './Content'
-import PropTypes from 'prop-types'
+import App from './App'
 import './index.css'
 
-function createStore (reducer) {
-  let state = null
-  const listeners = []
-  const subscribe = function (listener) {
-    listeners.push(listener)
-  }
-  const getState = function () {
-    return state
-  }
-  const dispatch = function (action) {
-    state = reducer(state, action)
-    listeners.forEach((listener) => listener())
-  }
-  dispatch({})
-  return {
-    getState,
-    dispatch,
-    subscribe
-  }
-}
-
-const themeReducer = function(state, action) {
-  if (!state) {
-    return {
-      themeColor: 'red'
-    }
-  }
-  switch (action.type) {
-    case 'change_color':
-      return {
-        ...state,
-        themeColor: action.themeColor
-      }
-    default:
-      return state
-  }
-}
-
-const store = createStore(themeReducer)
-
-class Index extends Component {
-  static childContextTypes = {
-    store: PropTypes.object
-  }
-
-  getChildContext () {
-    return { store }
-  }
-
-  render () {
-    return (
-      <div>
-        <Header/>
-        <Content/>
-      </div>
-    )
-  }
-}
-
 ReactDOM.render(
-  <Index/>,
+  <App/>,
   document.getElementById('root')
 )
